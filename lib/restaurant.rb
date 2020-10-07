4470741409e3f72397c82539d588079a04707f39
Saundra <saundracatalina@gmail.com>
2020-10-07 14:26:09 -0600

    Did not complete closing time test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,14 +1,14 @@
 class Restaurant
-  attr_reader :opening_time, :name, :dishes, :hours_open
+  attr_reader :opening_time, :name, :dishes, :hours_open, :closing_time
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
-    @closing_time = (opening_time + hours_open)
+    @closing_time = closing_time
   end
 
   def closing_time(hours_open)
-    @closing_time += hours_open
+    @closing_time = (opening_time + hours_open)
   end
 
   def add_dish(dish)
